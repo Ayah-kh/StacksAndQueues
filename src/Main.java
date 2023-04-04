7a9a4d03242a9a48f7c54e738ca8e124cc18f06b
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-04-04 13:54:57 +0300

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -14,5 +14,19 @@ public class Main {
         System.out.println(stack.pop());
         System.out.println(stack.isEmpty());
 
+        System.out.println("-------------------");
+
+        FixedCapacityStackOfStrings stack1=new FixedCapacityStackOfStrings(10);
+        stack1.push("Hello");
+        stack1.push("My World");
+        stack1.push("How");
+        stack1.push("Please");
+        System.out.println(stack1.isEmpty());
+        System.out.println(stack1.pop());
+        System.out.println(stack1.pop());
+        System.out.println(stack1.pop());
+        System.out.println(stack1.pop());
+        System.out.println(stack1.isEmpty());
+
     }
 }
