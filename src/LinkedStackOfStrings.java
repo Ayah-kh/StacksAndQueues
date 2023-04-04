c6a03497049614128dce78f38f39946afff3d22f
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-04-04 13:36:04 +0300

    First Commit

--- src/LinkedStackOfStrings.java
+++ src/LinkedStackOfStrings.java
@@ -1,2 +1,17 @@
 public class LinkedStackOfStrings {
+
+    private Node first=null;
+
+    private class Node{
+        String item;
+        Node Next;
+    }
+
+    public boolean isEmpty(){
+        return first==null;
+    }
+
+    public void push(String item){
+        Node oldFirst=first;
+    }
 }
