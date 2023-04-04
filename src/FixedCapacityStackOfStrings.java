aa4cad84e001cd34f596b1bf31e0537427f26645
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-04-04 13:52:38 +0300

    First Commit

--- src/FixedCapacityStackOfStrings.java
+++ src/FixedCapacityStackOfStrings.java
@@ -1,2 +1,20 @@
 public class FixedCapacityStackOfStrings {
+    public String[] strings;
+    private int N = 0;
+
+    public FixedCapacityStackOfStrings(int capacity) {
+        strings = new String[capacity];
+    }
+
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    public void push(String item){
+        strings[N++]=item;
+    }
+
+    public String pop(){
+        return strings[--N];
+    }
 }
