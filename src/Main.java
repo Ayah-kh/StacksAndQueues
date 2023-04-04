1e8f223c0341c648eb16aedcc57f2d70f4167369
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-04-04 16:11:32 +0300

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -2,47 +2,9 @@ import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
-        LinkedStackOfStrings stack=new LinkedStackOfStrings();
+        StackOfObjects<String> stack=new StackOfObjects<>();
         stack.push("Hello");
-        stack.push("My World");
-        stack.push("How");
-        stack.push("Please");
-        System.out.println(stack.isEmpty());
         System.out.println(stack.pop());
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
-        System.out.println(stack.isEmpty());
-
-        System.out.println("-------------------");
-
-        FixedCapacityStackOfStrings stack1=new FixedCapacityStackOfStrings(10);
-        stack1.push("Hello");
-        stack1.push("My World");
-        stack1.push("How");
-        stack1.push("Please");
-        System.out.println(stack1.isEmpty());
-        System.out.println(stack1.pop());
-        System.out.println(stack1.pop());
-        System.out.println(stack1.pop());
-        System.out.println(stack1.pop());
-        System.out.println(stack1.isEmpty());
-
-        System.out.println("----------------");
-        LinkedQueueOfStrings queue=new LinkedQueueOfStrings();
-        queue.enqueue("Hello");
-        queue.enqueue("sad");
-        queue.enqueue("happy");
-        queue.enqueue("world");
-        queue.enqueue("!!!");
-        System.out.println(queue.isEmpty());
-        System.out.println(queue.dequeue());
-        System.out.println(queue.dequeue());
-        System.out.println(queue.dequeue());
-        System.out.println(queue.dequeue());
-        System.out.println(queue.dequeue());
-
-        System.out.println(queue.isEmpty());
 
 
     }
